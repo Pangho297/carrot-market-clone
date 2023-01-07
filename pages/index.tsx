112381e756c6e757087552e8f994a198e2719538
Pangho297 <monanazzang@gmail.com>
2023-01-08 02:17:07 +0900

    ADD: Component 3 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="bg-slate-400 py-20 px-20 grid gap-5">
+    <div className="bg-slate-400 py-20 px-20 grid gap-5 min-h-screen">
       <div className="bg-white p-6 rounded-3xl shadow-2xl flex flex-col items-center">
         <span className="w-full text-left font-semibold text-3xl">
           Select Item
@@ -43,8 +43,42 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <div className="bg-white p-10 rounded-2xl shadow-2xl"></div>
-      <div className="bg-white p-10 rounded-2xl shadow-2xl"></div>
+      <div className="bg-white p-10 rounded-2xl shadow-2xl">
+        <div className="flex mb-5 justify-between items-center">
+          <span>⬅️</span>
+          <div className="space-x-3">
+            <span>⭐️ 4.9</span>
+            <span className="shadow-xl p-2 rounded-md">💖</span>
+          </div>
+        </div>
+        <div className="bg-zinc-400 h-72 mb-5" />
+        <div className="flex flex-col">
+          <span className="font-medium text-xl">Swoon Lounge</span>
+          <span className="text-xs text-gray-500">Chair</span>
+          <div className="mt-3 mb-5 flex justify-between items-center">
+            <div className="space-x-2">
+              <button className="w-5 h-5 rounded-full bg-yellow-400"></button>
+              <button className="w-5 h-5 rounded-full bg-pink-400"></button>
+              <button className="w-5 h-5 rounded-full bg-blue-400"></button>
+            </div>
+            <div className="flex items-center space-x-5">
+              <button className="rounded-lg bg-blue-200 flex justify-center items-center aspect-square w-8 text-xl text-gray-500">
+                -
+              </button>
+              <span>1</span>
+              <button className="rounded-lg bg-blue-200 flex justify-center items-center aspect-square w-8 text-xl text-gray-500">
+                +
+              </button>
+            </div>
+          </div>
+          <div className="flex justify-between items-center">
+            <span className="font-medium text-2xl">$450</span>
+            <button className="py-2 px-8 bg-blue-500 text-xs text-white rounded-lg">
+              Add to cart
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
