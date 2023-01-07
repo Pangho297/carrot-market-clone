d03ef4508da32b683f4fc3984cfbcfd37081735c
Pangho297 <monanazzang@gmail.com>
2023-01-08 00:05:05 +0900

    ADD: Component 2 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div className="bg-slate-400 py-10 px-5 grid gap-5">
-      <div className="bg-white p-10 rounded-3xl shadow-2xl flex flex-col items-center">
+    <div className="bg-slate-400 py-20 px-20 grid gap-5">
+      <div className="bg-white p-6 rounded-3xl shadow-2xl flex flex-col items-center">
         <span className="w-full text-left font-semibold text-3xl">
           Select Item
         </span>
@@ -21,7 +21,28 @@ export default function Home() {
           Checkout
         </button>
       </div>
-      <div className="bg-white p-10 rounded-2xl shadow-2xl"></div>
+      <div className="bg-white overflow-hidden rounded-2xl shadow-2xl">
+        <div className="bg-blue-500 p-6 pb-14">
+          <span className="text-white text-2xl">Profile</span>
+        </div>
+        <div className="rounded-3xl p-6 bg-white relative -top-5">
+          <div className="flex relative -top-16 justify-between items-end">
+            <div className="flex flex-col items-center">
+              <span className="text-sm text-gray-500">Orders</span>
+              <span className="font-medium">340</span>
+            </div>
+            <div className="h-24 w-24 rounded-full bg-yellow-400" />
+            <div className="flex flex-col items-center">
+              <span className="text-sm text-gray-500">Spent</span>
+              <span className="font-medium">$2,310</span>
+            </div>
+          </div>
+          <div className="relative -mt-10 -mb-5 flex flex-col items-center">
+            <span className="text-lg font-medium">Pangho</span>
+            <span className="text-sm text-gray-500">미국</span>
+          </div>
+        </div>
+      </div>
       <div className="bg-white p-10 rounded-2xl shadow-2xl"></div>
       <div className="bg-white p-10 rounded-2xl shadow-2xl"></div>
     </div>
