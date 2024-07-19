27df6253268d16d12f57f31387a88c91f4ecf522
Pangho297 <monanazzang@gmail.com>
2024-07-20 01:11:04 +0900

    feature: Route Handler 추가

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,8 +1,22 @@
+"use client"; // test
+
 import FormButton from "@/components/form-btn";
 import FormInput from "@/components/form-input";
 import SocialLogin from "@/components/social-login";
 
 export default function Login() {
+  const onClick = async () => {
+    const res = await fetch("/api/users", {
+      method: "POST",
+      body: JSON.stringify({
+        username: "pangho",
+        password: "you should not see my password!",
+      }),
+    });
+
+    console.log(await res.json());
+  };
+
   return (
     <div className="flex flex-col gap-10 py-6">
       <div className="flex flex-col gap-2 *:font-medium">
@@ -25,7 +39,9 @@ export default function Login() {
           errorMessage={["비밀번호를 입력해 주세요"]}
         />
 
-        <FormButton disabled={false}>로그인</FormButton>
+        <FormButton disabled={false} onClick={onClick}>
+          로그인
+        </FormButton>
       </form>
       {/* 구분선 */}
       <SocialLogin />
