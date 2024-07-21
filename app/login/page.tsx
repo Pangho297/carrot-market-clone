049cd3bae18caffbe28b23ed13162a7ae1724570
Pangho297 <monanazzang@gmail.com>
2024-07-21 21:13:45 +0900

    feature: Server Action

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,21 +1,13 @@
-"use client"; // test
-
 import FormButton from "@/components/form-btn";
 import FormInput from "@/components/form-input";
 import SocialLogin from "@/components/social-login";
 
 export default function Login() {
-  const onClick = async () => {
-    const res = await fetch("/api/users", {
-      method: "POST",
-      body: JSON.stringify({
-        username: "pangho",
-        password: "you should not see my password!",
-      }),
-    });
+  async function onSubmit(formData: FormData) {
+    "use server"; // Server Action을 사용하기 위한 선언, 서버에서만 실행된다
 
-    console.log(await res.json());
-  };
+    console.log(formData.get("email"), formData.get("password"));
+  }
 
   return (
     <div className="flex flex-col gap-10 py-6">
@@ -25,23 +17,23 @@ export default function Login() {
           로그인을 위해 이메일과 비밀번호를 입력해주세요!
         </h2>
       </div>
-      <form className="flex flex-col gap-3">
+      <form className="flex flex-col gap-3" action={onSubmit}>
         <FormInput
           type="email"
+          name="email"
           placeholder="이메일을 입력해 주세요."
           required
           errorMessage={["이메일 형식으로 입력해 주세요"]}
         />
         <FormInput
           type="password"
+          name="password"
           placeholder="비밀번호를 입력해 주세요"
           required
           errorMessage={["비밀번호를 입력해 주세요"]}
         />
 
-        <FormButton disabled={false} onClick={onClick}>
-          로그인
-        </FormButton>
+        <FormButton disabled={false}>로그인</FormButton>
       </form>
       {/* 구분선 */}
       <SocialLogin />
