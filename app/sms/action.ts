0ca12546e5c0a278619c3da96c5c6de6ccbce583
Pangho297 <monanazzang@gmail.com>
2024-07-23 19:25:52 +0900

    feature: sms validation add

--- app/sms/action.ts
+++ app/sms/action.ts
@@ -2,17 +2,50 @@
 
 import { z } from "zod";
 import validator from "validator";
+import { redirect } from "next/navigation";
+
+interface ActionState {
+  token: boolean;
+}
 
 const phoneSchema = z
   .string()
   .trim()
-  .refine((value) => validator.isMobilePhone(value, "ko-KR"));
+  .refine(
+    (phone) => validator.isMobilePhone(phone, "ko-KR"),
+    "휴대전화번호가 잘못되었습니다"
+  );
 
 const tokenSchema = z.coerce.number().min(100000).max(999999);
 
-export async function smsLogin(prev: any, formData: FormData) {
-  console.log(formData);
+export async function smsLogin(prev: ActionState, formData: FormData) {
+  const phone = formData.get("phone");
+  const token = formData.get("token");
 
-  console.log(typeof formData.get("token"));
-  console.log(typeof tokenSchema.parse(formData.get("token")));
+  if (!prev.token) {
+    // 사용자가 전화번호만 입력한 경우
+    const result = phoneSchema.safeParse(phone);
+
+    // 휴대전화 유효성 검사
+    if (!result.success) {
+      console.log(result.error);
+      // 실패
+      return { token: false, error: result.error.flatten() };
+    } else {
+      // 통과
+      return { token: true };
+    }
+  } else {
+    // 사용자가 토큰도 입력한 경우
+    const result = tokenSchema.safeParse(token);
+
+    // 토큰 유효성 검사
+    if (!result.success) {
+      // 실패
+      return { token: true, error: result.error.flatten() }; // FIXME: 에러처리 필요
+    } else {
+      // 성공
+      redirect("/");
+    }
+  }
 }
--- app/sms/page.tsx
+++ app/sms/page.tsx
@@ -6,8 +6,15 @@ import SocialLogin from "@/components/social-login";
 import { useFormState } from "react-dom";
 import { smsLogin } from "./action";
 
+const initialState = {
+  token: false,
+  error: undefined,
+};
+
 export default function SMSLogin() {
-  const [state, dispatch] = useFormState(smsLogin, null);
+  const [state, dispatch] = useFormState(smsLogin, initialState);
+
+  console.log(state);
 
   return (
     <div className="flex flex-col gap-10 py-6">
@@ -16,24 +23,29 @@ export default function SMSLogin() {
         <h2 className="text-xl">휴대전화번호를 인증해주세요</h2>
       </div>
       <form className="flex flex-col gap-3" action={dispatch}>
-        <Input
-          type="text"
-          name="phone"
-          placeholder="휴대전화번호를 입력해 주세요"
-          required
-          errorMessage={[]}
-        />
-        <Input
-          type="number"
-          name="token"
-          placeholder="인증번호를 입력해 주세요"
-          required
-          min={100000}
-          max={999999}
-          errorMessage={[]}
-        />
+        {state?.token ? (
+          <Input
+            key="token"
+            type="number"
+            name="token"
+            placeholder="인증번호를 입력해 주세요"
+            required
+            min={100000}
+            max={999999}
+            errorMessage={state.error?.formErrors}
+          />
+        ) : (
+          <Input
+            key="phone"
+            type="text"
+            name="phone"
+            placeholder="휴대전화번호를 입력해 주세요"
+            required
+            errorMessage={state.error?.formErrors}
+          />
+        )}
 
-        <Button>인증</Button>
+        <Button>{state.token ? "인증" : "인증토큰 발송"}</Button>
       </form>
     </div>
   );
