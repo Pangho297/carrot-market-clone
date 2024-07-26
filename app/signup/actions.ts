1eb5ba759dfc9232731b3af4f7fd6bdd1c88bcfc
Pangho297 <monanazzang@gmail.com>
2024-07-26 22:44:41 +0900

    feature: superRefine add

--- app/signup/actions.ts
+++ app/signup/actions.ts
@@ -12,8 +12,6 @@ import {
 import db from "@/lib/db";
 import { z } from "zod";
 import bcrypt from "bcrypt";
-import { getIronSession } from "iron-session";
-import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 import getSession from "@/lib/session";
 
@@ -26,33 +24,32 @@ interface FormSchema {
 
 const checkPasswords = ({ password, confirm_password }: FormSchema) =>
   password === confirm_password;
-
 // 유효성을 통과한 경우 DB에 동일한 username, email이 존재하는지 확인
-const checkUniqueUsername = async (username: string) => {
-  const user = await db.user.findUnique({
-    where: {
-      username,
-    },
-    select: {
-      id: true,
-    },
-  });
+// const checkUniqueUsername = async (username: string) => {
+//   const user = await db.user.findUnique({
+//     where: {
+//       username,
+//     },
+//     select: {
+//       id: true,
+//     },
+//   });
 
-  return !Boolean(user);
-};
+//   return !Boolean(user);
+// };
 
-const checkUniqueEmail = async (email: string) => {
-  const user = await db.user.findUnique({
-    where: {
-      email,
-    },
-    select: {
-      id: true,
-    },
-  });
+// const checkUniqueEmail = async (email: string) => {
+//   const user = await db.user.findUnique({
+//     where: {
+//       email,
+//     },
+//     select: {
+//       id: true,
+//     },
+//   });
 
-  return !Boolean(user);
-};
+//   return !Boolean(user);
+// };
 
 const formSchema: z.ZodType<FormSchema> = z
   .object({
@@ -62,16 +59,16 @@ const formSchema: z.ZodType<FormSchema> = z
         invalid_type_error: "사용자 이름이 잘못되었습니다 다시 확인해주세요",
       })
       .toLowerCase() // 모든 문자를 소문자로 변경
-      .trim() // 문자열 양끝의 공백을 제거
-      // .transform((value) => `test_${value}_test`), // 데이터를 임의로 변경하여 반환함
-      .refine(checkUniqueUsername, "이미 존재하는 이름입니다"), // 단일 항목에 대한 검증 함수
+      .trim(), // 문자열 양끝의 공백을 제거
+    // .transform((value) => `test_${value}_test`), // 데이터를 임의로 변경하여 반환함
+    // .refine(checkUniqueUsername, "이미 존재하는 이름입니다"), // 단일 항목에 대한 검증 함수
     email: z
       .string({
         required_error: EMAIL_REQUIRED_ERROR,
       })
       .email(EMAIL_TYPE_ERROR)
-      .toLowerCase()
-      .refine(checkUniqueEmail, "이미 존재하는 이메일입니다"),
+      .toLowerCase(),
+    // .refine(checkUniqueEmail, "이미 존재하는 이메일입니다"),
     password: z
       .string({
         required_error: PASSWORD_REQUIRED_ERROR,
@@ -80,13 +77,51 @@ const formSchema: z.ZodType<FormSchema> = z
       .regex(PASSWORD_REGEX, PASSWORD_REGEX_ERROR),
     confirm_password: z.string().min(PASSWORD_MIN_LENGTH),
   })
+  .superRefine(async ({ username }, context) => {
+    const user = await db.user.findUnique({
+      where: {
+        username,
+      },
+      select: {
+        id: true,
+      },
+    });
+
+    if (user) {
+      context.addIssue({
+        code: "custom",
+        message: "이미 존재하는 이름입니다.",
+        path: ["username"],
+        fatal: true,
+      });
+      return z.NEVER;
+    }
+  })
+  .superRefine(async ({ email }, context) => {
+    const user = await db.user.findUnique({
+      where: {
+        email,
+      },
+      select: {
+        id: true,
+      },
+    });
+    if (user) {
+      context.addIssue({
+        code: "custom",
+        message: "이미 존재하는 이메일입니다.",
+        path: ["email"],
+        fatal: true,
+      });
+      return z.NEVER;
+    }
+  })
   .refine(checkPasswords, {
     message: "비밀번호가 일치하지 않습니다",
     path: ["confirm_password"], // 에러가 표시될 위치 지정
   });
 
 export async function createAccount(prev: any, formData: FormData) {
-  console.log(cookies());
   const data = {
     username: formData.get("username"),
     email: formData.get("email"),
