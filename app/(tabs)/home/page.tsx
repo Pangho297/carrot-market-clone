70e98e1af8c634dfaa053ce527c0962cf84b31c0
Pangho297 <monanazzang@gmail.com>
2024-08-19 14:49:45 +0900

    feature: revalidatePath add

--- app/(tabs)/home/page.tsx
+++ app/(tabs)/home/page.tsx
@@ -4,10 +4,12 @@ import { PAGE_LIMIT } from "@/lib/constants";
 import db from "@/lib/db";
 import { PlusIcon } from "@heroicons/react/24/solid";
 import { Prisma } from "@prisma/client";
-import { unstable_cache as nextCache } from "next/cache";
+import { unstable_cache as nextCache, revalidatePath } from "next/cache";
 import Link from "next/link";
 
-const getCachedProducts = nextCache(getInitialProducts, ["home-products"]);
+const getCachedProducts = nextCache(getInitialProducts, ["home-products"], {
+  revalidate: 60,
+});
 
 async function getInitialProducts() {
   const products = await db.product.findMany({
@@ -33,9 +35,17 @@ export type InitialProducts = Prisma.PromiseReturnType<
 
 export default async function Products() {
   const initialProducts = await getCachedProducts();
+  const revalidate = async () => {
+    "use server";
+    revalidatePath("/home")
+
+  }
   return (
     <div>
       <ProductList initialProducts={initialProducts} />
+      <form action={revalidate}>
+        <button>캐시 새로고침 테스트</button>
+      </form>
       <Link
         href="/products/add"
         className="fixed bottom-24 right-8 flex size-16 items-center justify-center rounded-full bg-orange-500 text-white transition-colors hover:bg-orange-400"
