bf5dbb51bf0267df960ea1ca10b6a8d52f06b45f
Pangho297 <monanazzang@gmail.com>
2024-08-28 18:03:33 +0900

    feature: server-only add

--- app/extras/page.tsx
+++ app/extras/page.tsx
@@ -1,24 +1,22 @@
-import { revalidatePath } from "next/cache";
+import HackedComponent from "@/components/HackedComponent";
+import { experimental_taintObjectReference } from "react";
 
-async function getData() {
-  const data = await fetch(
-    "https://nomad-movies.nomadcoders.workers.dev/movies"
-  );
+function getData() {
+  const secret = {
+    apiKey: "12345678",
+    secret: "01010110",
+  };
+  experimental_taintObjectReference("API Keys 가 유출되었습니다!!!!", secret);
+  return secret;
 }
 
 export default async function Extras() {
-  await getData();
-  const action = async () => {
-    "use server";
-    revalidatePath("/extras");
-  };
+  const data = getData();
   return (
     <div className="flex flex-col gap-3 py-10">
       <h1 className="font-metallica text-6xl">Extras!</h1>
       <h2 className="font-roboto">Next.js의 추가 기능들</h2>
-      <form action={action}>
-        <button>캐시 메모리 초기화</button>
-      </form>
+      <HackedComponent data={data} />
     </div>
   );
 }
--- components/HackedComponent.tsx
+++ components/HackedComponent.tsx
@@ -1,6 +1,9 @@
 "use client";
 
+import { fetchFormAPI } from "@/app/extras/actions";
+
 /** 클라이언트 컴포넌트로의 데이터 누출 방지 학습용 컴포넌트  */
 export default function HackedComponent({}: any) {
+  fetchFormAPI()
   return <h1>해킹!</h1>
 }
