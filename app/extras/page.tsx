7ab85f7dc084a6a7f528a5c5cf9aae27040f6e1b
Pangho297 <monanazzang@gmail.com>
2024-08-28 18:33:20 +0900

    feature: image placeholder add

--- app/extras/page.tsx
+++ app/extras/page.tsx
@@ -1,22 +1,10 @@
-import HackedComponent from "@/components/HackedComponent";
-import { experimental_taintObjectReference } from "react";
-
-function getData() {
-  const secret = {
-    apiKey: "12345678",
-    secret: "01010110",
-  };
-  experimental_taintObjectReference("API Keys 가 유출되었습니다!!!!", secret);
-  return secret;
-}
+import Image from "next/image";
+import HeavyImage from "../../public/heavy-image.jpeg";
 
 export default async function Extras() {
-  const data = getData();
   return (
     <div className="flex flex-col gap-3 py-10">
-      <h1 className="font-metallica text-6xl">Extras!</h1>
-      <h2 className="font-roboto">Next.js의 추가 기능들</h2>
-      <HackedComponent data={data} />
+      <Image src={HeavyImage} alt="" placeholder="blur" />
     </div>
   );
 }
